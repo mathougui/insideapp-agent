6fed33d4bf01816c14bd79ff12e54a329e497aef
McBow <rcendrier@yahoo.fr>
2018-02-21 15:51:58 +0100

    Optimized resources gathering

--- agent/network.py
+++ agent/network.py
@@ -19,24 +19,25 @@ class ApiCall:
 
     def fill_payload(self):
         payload = {'api_key': sys.argv[1]}
-        if self.resources_to_get['cpu_process']:
-            cpu_process = self.resources.get_process_cpu_percent()
-            payload['cpu_process'] = cpu_process
-        if self.resources_to_get['cpu_global']:
-            cpu_global = self.resources.get_global_cpu_percent()
-            payload['cpu_global'] = cpu_global
-        if self.resources_to_get['memory_process']:
-            memory_process = self.resources.get_process_memory_percent()
-            payload['memory_process'] = memory_process
-        if self.resources_to_get['memory_global']:
-            memory_global = self.resources.get_global_memory_percent()
-            payload['memory_global'] = memory_global
-        if self.resources_to_get['swap_process']:
-            swap_process = self.resources.get_process_swap_percent()
-            payload['swap_process'] = swap_process
-        if self.resources_to_get['swap_global']:
-            swap_global = self.resources.get_global_swap_percent()
-            payload['swap_global'] = swap_global
+        with self.resources.process.oneshot():
+            if self.resources_to_get['cpu_process']:
+                cpu_process = self.resources.get_process_cpu_percent()
+                payload['cpu_process'] = cpu_process
+            if self.resources_to_get['cpu_global']:
+                cpu_global = self.resources.get_global_cpu_percent()
+                payload['cpu_global'] = cpu_global
+            if self.resources_to_get['memory_process']:
+                memory_process = self.resources.get_process_memory_percent()
+                payload['memory_process'] = memory_process
+            if self.resources_to_get['memory_global']:
+                memory_global = self.resources.get_global_memory_percent()
+                payload['memory_global'] = memory_global
+            if self.resources_to_get['swap_process']:
+                swap_process = self.resources.get_process_swap_percent()
+                payload['swap_process'] = swap_process
+            if self.resources_to_get['swap_global']:
+                swap_global = self.resources.get_global_swap_percent()
+                payload['swap_global'] = swap_global
         print(payload)
         return payload
 
