5ec63df4122df71d3e054e97de2848e3d100e6db
McBow <rcendrier@yahoo.fr>
2018-03-20 14:43:12 +0100

    send static resources when agent is started

--- agent/mainLoop.py
+++ agent/mainLoop.py
@@ -13,25 +13,32 @@ from resources import Resources
 
 class MainLoop:
     resources = None
-    resources_names = []
+    dynamic_resources_names = []
+    static_resources_names = []
     resources_to_get = {}
     logs_to_get = {"nginx": "/var/log/nginx/error.log"}
-    resources_functions = {}
+    dynamic_resources_functions = {}
+    static_resources_functions = {}
     api_url = "http://localhost:3000"
     logs_url = api_url + "/api/v1/logs"
-    resources_url = api_url + "/api/v1/metrics"
+    dynamic_resources_url = api_url + "/api/v1/metrics"
+    static_resources_url = api_url + "/api/v1/metrics/static"
 
     def fill_resources_to_get(self):
-        for resource_name in self.resources_names:
+        for resource_name in self.dynamic_resources_names:
             # TODO Get the real values from API
             self.resources_to_get[resource_name] = True
 
     def read_config_file(self):
         with open('config.json') as config_file:
             try:
-                self.resources_names = json.load(config_file)["resources"]
-                for resource in self.resources_names:
-                    self.resources_functions[resource] = getattr(self.resources, "get_" + resource)
+                config_file = json.load(config_file)
+                self.dynamic_resources_names = config_file["dynamic_resources"]
+                self.static_resources_names = config_file["static_resources"]
+                for resource in self.dynamic_resources_names:
+                    self.dynamic_resources_functions[resource] = getattr(self.resources, "get_" + resource)
+                for resource in self.static_resources_names:
+                    self.static_resources_functions[resource] = getattr(self.resources, "get_" + resource)
             except KeyError:
                 exit(1)
 
@@ -46,7 +53,22 @@ class MainLoop:
             self.api_url = os.environ["API_URL"]
 
     def launch_main_loop(self):
+        self.send_static_resources()
         self.get_resources_and_logs()
+
+    def send_static_resources(self):
+        payload = {}
+        try:
+            for r in self.static_resources_names:
+                try:
+                    resource = self.static_resources_functions[r]()
+                    payload[r] = resource
+                except IndexError:
+                    pass
+        except psutil.NoSuchProcess:
+            print('Could not find process "' +
+                  self.resources.process_name + '"')
+        self.make_request(payload, self.static_resources_url)
 
     def get_resources_and_logs(self):
         while True:
@@ -57,7 +79,7 @@ class MainLoop:
 
     def send_resources(self):
         payload = self.get_all_needed_resources()
-        self.make_request(payload, self.resources_url)
+        self.make_request(payload, self.dynamic_resources_url)
 
     def get_all_needed_resources(self):
         payload = {}
@@ -65,7 +87,7 @@ class MainLoop:
             with self.resources.process.oneshot():
                 for p in self.resources_to_get:
                     try:
-                        resource = self.resources_functions[p]()
+                        resource = self.dynamic_resources_functions[p]()
                         payload[p] = resource
                     except IndexError:
                         pass
