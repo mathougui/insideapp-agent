d7b2eb24cff8c854586d3a448050244d894bb646
McBow <rcendrier@yahoo.fr>
2018-03-29 10:30:29 +0200

    Add tests for global cpu metrics

---
+++ tests/test_resources.py
@@ -0,0 +1,23 @@
+import unittest
+
+from agent import resources
+
+
+class ResourceTest(unittest.TestCase):
+    resource = resources.Resources("python")
+
+    def test_cpu_time_user(self):
+        self.assertGreater(self.resource.get_cpu_time_user(), 0)
+
+    def test_cpu_time_system(self):
+        self.assertGreater(self.resource.get_cpu_time_system(), 0)
+
+    def test_cpu_time_idle(self):
+        self.assertGreaterEqual(self.resource.get_cpu_time_idle(), 0)
+
+    def test_cpu_percent(self):
+        self.assertGreaterEqual(self.resource.get_cpu_percent(), 0)
+        self.assertLessEqual(self.resource.get_cpu_percent(), 100)
+
+    def test_cpu_freq_current(self):
+        self.assertGreaterEqual(self.resource.get_cpu_freq_current(), 0)
