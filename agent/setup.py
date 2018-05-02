680070070b670047fa6a51d2e7360839592e60a0
McBow <rcendrier@yahoo.fr>
2018-05-02 11:18:13 +0200

    Add dependency for packaging

--- agent/setup.py
+++ agent/setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 build_exe_options = {
-    "packages": ['idna.idnadata', 'appdirs', 'packaging.specifiers', 'packaging.requirements', 'psutil'],
+    "packages": ['six', 'idna.idnadata', 'appdirs', 'packaging.specifiers', 'packaging.requirements', 'psutil'],
     "include_files": ["config.json"]
 }
 
